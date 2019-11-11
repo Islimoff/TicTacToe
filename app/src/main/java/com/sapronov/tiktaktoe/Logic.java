0659fad430f309703c0202cb6835adf6e7380f4c
unknown <islimoff@gmail.com>
2019-11-11 11:21:32 +0200

    added logic

--- app/src/main/java/com/sapronov/tiktaktoe/Logic.java
+++ app/src/main/java/com/sapronov/tiktaktoe/Logic.java
@@ -1,5 +1,22 @@
 package com.sapronov.tiktaktoe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logic {
 
+    private final List<Integer> buttons;
+    private final List<Integer> answers = new ArrayList<>();
+
+    public Logic(List<Integer> buttons) {
+        this.buttons = buttons;
+    }
+
+    public void addAnswer(Integer id) {
+        answers.add(id);
+    }
+
+    public boolean checkWin(){
+       return true;
+    }
 }
--- app/src/main/java/com/sapronov/tiktaktoe/MainActivity.java
+++ app/src/main/java/com/sapronov/tiktaktoe/MainActivity.java
@@ -7,25 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
-    int counter=1;
+    private int counter = 0;
+    private Logic logic = new Logic(Arrays.asList(R.id.button1, R.id.button2, R.id.button3, R.id.button4
+            , R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void answer(View view)
-    {
-        Button button= (Button) view;
-        if (counter%2==0){
+    public void answer(View view) {
+        Button button = (Button) view;
+        logic.addAnswer(button.getId());
+        if (counter % 2 == 0) {
             button.setText("O");
-        }
-        else{
+        } else {
             button.setText("X");
         }
+        button.setClickable(false);
         counter++;
     }
 }
